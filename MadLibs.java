df61bb36e019ca30d1b232f5d0ce2bd4289f01cc
lexigreenfield <alexis.greenfield@student.tamdistrict.org>
2024-09-26 09:59:47 -0700

    first required lines

--- MadLibs.java
+++ MadLibs.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class MadLibs {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // System.out.println("hello world");
+        // System.out.println("My name is Lexi!");
+        // don't worry about scanners not being closed
+        /*
+         * Scanner scanner = new Scanner(System.in);
+         * System.out.println("Enter a string: ");
+         * String input = scanner.nextLine();
+         * System.out.println("You said: " + input);
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        String madlib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+        // madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        // madlib = "The <adjective> <noun> <verb>.";
     }
 
 }
